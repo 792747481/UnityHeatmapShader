b904b9d3c7c6bcb995cfedd7c51dd107302407bb
wangtao <17600149625@163.com>
2025-09-10 15:04:11 +0800

    改成随机点位

--- Assets/QuadScript.cs
+++ Assets/QuadScript.cs
@@ -1,78 +1,119 @@
-﻿using System.Collections;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class QuadScript : MonoBehaviour
+//{
+//  Material mMaterial;
+//  MeshRenderer mMeshRenderer;
+
+//  float[] mPoints;
+//  int mHitCount;
+
+//  float mDelay;
+
+
+//  void Start()
+//  {
+//    mDelay = 3;
+
+//    mMeshRenderer = GetComponent<MeshRenderer>();
+//    mMaterial = mMeshRenderer.material;
+
+//    mPoints = new float[32 * 3]; //32 point 
+
+//  }
+
+//  void Update()
+//  {
+//    mDelay -= Time.deltaTime;
+//    if (mDelay <=0)
+//    {
+//      GameObject go = Instantiate(Resources.Load<GameObject>("Projectile"));
+//      go.transform.position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-3f, -1f));
+
+//      mDelay = 0.5f;
+//    }
+
+//  }
+
+//  private void OnCollisionEnter(Collision collision)
+//  {
+//    foreach(ContactPoint cp in collision.contacts)
+//    {
+//      Debug.Log("Contact with object " + cp.otherCollider.gameObject.name);
+
+//      Vector3 StartOfRay = cp.point - cp.normal;
+//      Vector3 RayDir = cp.normal;
+
+//      Ray ray = new Ray(StartOfRay, RayDir);
+//      RaycastHit hit;
+
+//      bool hitit = Physics.Raycast(ray, out hit, 10f, LayerMask.GetMask("HeatMapLayer"));
+
+//      if (hitit)
+//      {
+//        Debug.Log("Hit Object " + hit.collider.gameObject.name);
+//        Debug.Log("Hit Texture coordinates = " + hit.textureCoord.x + "," + hit.textureCoord.y);
+//        addHitPoint(hit.textureCoord.x*4-2, hit.textureCoord.y*4-2);
+//      }
+//      Destroy(cp.otherCollider.gameObject);
+//    }
+//  }
+
+//  public void addHitPoint(float xp,float yp)
+//  {
+//    mPoints[mHitCount * 3] = xp;
+//    mPoints[mHitCount * 3 + 1] = yp;
+//    mPoints[mHitCount * 3 + 2] = Random.Range(1f, 3f);
+
+//    mHitCount++;
+//    mHitCount %= 32;
+
+//    mMaterial.SetFloatArray("_Hits", mPoints);
+//    mMaterial.SetInt("_HitCount", mHitCount);
+
+//  }
+
+//}
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteAlways] // 在编辑器和运行时都能更新
 public class QuadScript : MonoBehaviour
 {
-  Material mMaterial;
-  MeshRenderer mMeshRenderer;
+    Material mMaterial;
+    MeshRenderer mMeshRenderer;
 
-  float[] mPoints;
-  int mHitCount;
+    float[] mPoints;
+    int mHitCount;
 
-  float mDelay;
+    [SerializeField] int N = 10; // 固定点的数量
 
-
-  void Start()
-  {
-    mDelay = 3;
-
-    mMeshRenderer = GetComponent<MeshRenderer>();
-    mMaterial = mMeshRenderer.material;
-
-    mPoints = new float[32 * 3]; //32 point 
-
-  }
-
-  void Update()
-  {
-    mDelay -= Time.deltaTime;
-    if (mDelay <=0)
+    void Start()
     {
-      GameObject go = Instantiate(Resources.Load<GameObject>("Projectile"));
-      go.transform.position = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-3f, -1f));
-
-      mDelay = 0.5f;
+        Init();
     }
-
-  }
-
-  private void OnCollisionEnter(Collision collision)
-  {
-    foreach(ContactPoint cp in collision.contacts)
+    void Init()
     {
-      Debug.Log("Contact with object " + cp.otherCollider.gameObject.name);
+        mMeshRenderer = GetComponent<MeshRenderer>();
+        mMaterial = mMeshRenderer.sharedMaterial;
 
-      Vector3 StartOfRay = cp.point - cp.normal;
-      Vector3 RayDir = cp.normal;
+        mPoints = new float[32 * 3]; // 最多 32 点
+        mHitCount = Mathf.Min(N, 32); // 不能超过 32
 
-      Ray ray = new Ray(StartOfRay, RayDir);
-      RaycastHit hit;
+        // 随机生成 N 个点（分布在 [-2,2] 范围）
+        for (int i = 0; i < mHitCount; i++)
+        {
+            mPoints[i * 3] = Random.Range(-2f, 2f); // x
+            mPoints[i * 3 + 1] = Random.Range(-2f, 2f); // y
+            //mPoints[i * 3 + 2] = Random.Range(1f, 3f);  // 强度
+            mPoints[i * 3 + 2] = 1;
+        }
 
-      bool hitit = Physics.Raycast(ray, out hit, 10f, LayerMask.GetMask("HeatMapLayer"));
-
-      if (hitit)
-      {
-        Debug.Log("Hit Object " + hit.collider.gameObject.name);
-        Debug.Log("Hit Texture coordinates = " + hit.textureCoord.x + "," + hit.textureCoord.y);
-        addHitPoint(hit.textureCoord.x*4-2, hit.textureCoord.y*4-2);
-      }
-      Destroy(cp.otherCollider.gameObject);
+        // 传给材质
+        mMaterial.SetFloatArray("_Hits", mPoints);
+        mMaterial.SetInt("_HitCount", mHitCount);
     }
-  }
-
-  public void addHitPoint(float xp,float yp)
-  {
-    mPoints[mHitCount * 3] = xp;
-    mPoints[mHitCount * 3 + 1] = yp;
-    mPoints[mHitCount * 3 + 2] = Random.Range(1f, 3f);
-
-    mHitCount++;
-    mHitCount %= 32;
-
-    mMaterial.SetFloatArray("_Hits", mPoints);
-    mMaterial.SetInt("_HitCount", mHitCount);
-
-  }
-
 }
